a572c4a2bc4d3d5671a597a66f46e36041366ed9
levans1 <45011820+levans1@users.noreply.github.com>
2020-01-31 13:39:22 -0800

    Add files via upload

--- chamber_of_reflection_new.rb
+++ chamber_of_reflection_new.rb
@@ -1,19 +1,32 @@
 # Welcome to Sonic Pi v3.
-back="C:/Users/logan_evans/Downloads/backingtrack.wav"
-elio="C:/Users/logan_evans/Downloads/cmbyn_dialouge.wav"
+back="C:/Users/logan_evans/Downloads/audacity sounds/backingtrack.wav"
+elio="C:/Users/logan_evans/Downloads/audacity sounds/cmbyn_dialouge.wav"
+love="C:/Users/logan_evans/Downloads/audacity sounds/love.wav"
+friend="C:/Users/logan_evans/Downloads/audacity sounds/friend2.wav"
 
+x=1
 use_bpm 131
 use_synth :piano
+2.times do
+  sample love, rate:x, amp: 5
+  print sample_duration love
+  x=x-0.25
+  sleep 15.8
+end
 live_loop :backtrack do
-  8.times do
-    sample back, amp: 10
+  5.times do
+    sample back, amp: 5
     print sample_duration back
     sleep 32.8
+  end
+  1.times do
+    sample friend
+    print sample_duration friend
   end
   stop
 end
 live_loop :demarco do
-  8.times do
+  4.times do
     sample elio, amp: 5
     print sample_duration elio
     sleep 18.8
